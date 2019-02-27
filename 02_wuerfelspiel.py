944409008e7a36f0cd764e68b7c48c96a4d347bc
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-02-27 15:06:55 +0100

    created 02

--- 02_wuerfelspiel.py
+++ 02_wuerfelspiel.py
@@ -1,9 +1,9 @@
 #02_wuerfelspiel.py
-angabe  =("Wie oft soll gewürfelt werden")
-schaetzung = ("Welche Zahl wird gezogen")
+angabe = input("Wie oft soll gewürfelt werden")
+schaetzung = input("Welche Zahl wird gezogen")
 schaetzung = int(schaetzung)
 angabe = int(angabe)
-for zahl in range (0,anzahl)
+for zahl in range (0,angabe)
 	import random
     random.seed()
     zahl = random.randint(1,6)
