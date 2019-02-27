310014a3da51e44235b5340feb4b75a09b9397e2
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-02-27 15:11:03 +0100

    created 02

--- 02_wuerfelspiel.py
+++ 02_wuerfelspiel.py
@@ -1,11 +1,11 @@
 #02_wuerfelspiel.py
+import random
+random.seed()
 angabe = input("Wie oft soll gewürfelt werden")
-
 schaetzung = input("Welche Zahl wird gezogen")
 schaetzung = int(schaetzung)
 angabe = int(angabe)
-    import random
-    random.seed()
+    
 for i in range (0,angabe):
 	zahl = random.randint(1,6)
 	print(zahl)
