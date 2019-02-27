14fd5329e50ed3fe2feaf6aed95bec45baf6ff8a
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-02-27 14:39:03 +0100

    created

--- 01_my-age.py
+++ 01_my-age.py
@@ -1,6 +1,7 @@
-#01_myage.py
+#01_my-age.py
+
 name = input("Wie heißt du")
 alter = input("Wie alt bist du")
 alter = int(alter)
 alter = alter + 1
-print = ( name,"bist nächstes Jahr", alter2, "alt"
+print = ( name,"bist nächstes Jahr", alter2, "alt")
