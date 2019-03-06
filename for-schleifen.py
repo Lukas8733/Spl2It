712fb6f2483ecc341dc177a1a6496b085f716526
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-03-06 13:25:14 +0100

    (create)

---
+++ for-schleifen.py
@@ -0,0 +1,12 @@
+for i in 1,2,3,4,5:
+	print (i end=", ")
+for	i in -4,-3,-2,-1,0,1,2,3,4:
+	print(i)
+for i in "asd","klasus", 22,45,"peter",12
+	print(i)
+for i in range(0,9):
+	print (i)
+for i in range(0,17,3):
+	print (i)
+for i in range(0,9),3:
+	print (i)
