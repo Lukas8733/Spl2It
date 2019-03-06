66fe00a6b435b55e56398ea6592c06a25436938d
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-03-06 14:44:02 +0100

    (create)

--- lottozahlen2.py
+++ lottozahlen2.py
@@ -4,19 +4,23 @@ random.seed()
 tipp = input("Bitte Zahlen eingeben")
 tipps = tipp.split(",")
 tipps =[int(i) for i in tipps]
-
+tipps.sort()
 lottogezogen = []
 counter = 0
 while tipps != lottogezogen:
+	lottogezogen = []
 	lottozahlen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]
 	for i in range(1,7):
 		i = random.randint(0,len(lottozahlen)-1)
 		x = lottozahlen.pop(i)
 		lottogezogen.append(x)
+	lottogezogen.sort()
+	print(lottogezogen)
 	if(lottogezogen != tipps):
-		counter += 1
+		counter = counter + 1
 	if(lottogezogen == tipps):
 		print("Es hat",counter,"durchläufe gedauerz")
+		break
 
 	
-print("Die Zahlen lauten",lottogezogen)
+print("Die Zahlen lauten",lottogezogen,"und wurden nach",counter,"ziehungen gezogen")
