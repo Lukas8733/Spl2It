244b1c7c2f0e05ee3f72fedeb7d44dacbeeef8dd
Lukas Frewein <171wita05@o365.htl-leoben.at>
2019-03-06 13:39:53 +0100

    (create)

--- lottozahlen.py
+++ lottozahlen.py
@@ -3,11 +3,14 @@ import random
 random.seed()
 lottozahlen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]
 lottogezogen = []
-for i in range(0,6):
-	ziehung = random.randint(1,45)
-	x = lottozahlen(ziehung)
-	lottozahlen.remove(x)
-	lottogezogen.append(x)
+for i in range(1,7):
+	lottogezogen.append(random.randint(1,45))
+	
+for z in lottogezogen:
+	for o in lottozahlen:
+		if(z == o):
+			lottozahlen.remove(z)
+
 	
 print(lottogezogen)
 	
